40adfed526d2ed8dfbc1e46f8acbcbc4ba7caabc
NoorSidra <noorisidra050@gmail.com>
2026-01-17 15:02:39 +0500

    Updated City

--- City.cpp
+++ City.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "City.h"
-
 using namespace std;
 
-// Constructor
-City::City(int n) {
+City::City(int n, int z) {
     locations = n;
+    zones = z;
 
     roads = new int*[locations];
     for (int i = 0; i < locations; i++) {
@@ -14,20 +13,22 @@ City::City(int n) {
             roads[i][j] = 0;   // no road initially
         }
     }
+
+    zoneOfLocation = new int[locations];
+    for (int i = 0; i < locations; i++) {
+        zoneOfLocation[i] = 0;
+    }
 }
 
-// Add road
 void City::addRoad(int a, int b, int d) {
     roads[a][b] = d;
-    roads[b][a] = d;   // undirected graph
+    roads[b][a] = d;
 }
 
-// Get direct distance
 int City::getDistance(int a, int b) {
     return roads[a][b];
 }
 
-// Display city map
 void City::display() {
     for (int i = 0; i < locations; i++) {
         for (int j = 0; j < locations; j++) {
@@ -37,11 +38,107 @@ void City::display() {
     }
 }
 
-// Destructor
+void City::setZone(int loc, int z) {
+    zoneOfLocation[loc] = z;
+}
+
+int City::getZone(int loc) {
+    return zoneOfLocation[loc];
+}
+
+int City::shortestPath(int start, int end) {
+    int *dist = new int[locations];
+    int *visited = new int[locations];
+
+    for (int i = 0; i < locations; i++) {
+        dist[i] = 1e9;
+        visited[i] = 0;
+    }
+
+    dist[start] = 0;
+
+    for (int k = 0; k < locations - 1; k++) {
+        int u = -1;
+        int minDist = 1e9;
+
+        for (int i = 0; i < locations; i++) {
+            if (!visited[i] && dist[i] < minDist) {
+                minDist = dist[i];
+                u = i;
+            }
+        }
+
+        if (u == -1) break;
+        visited[u] = 1;
+
+        for (int v = 0; v < locations; v++) {
+            if (roads[u][v] != 0 && !visited[v]) {
+                int newDist = dist[u] + roads[u][v];
+                if (newDist < dist[v]) {
+                    dist[v] = newDist;
+                }
+            }
+        }
+    }
+
+    int ans = dist[end];
+    delete[] dist;
+    delete[] visited;
+    return ans;
+}
+
+int City::shortestPathWithZoneCost(int start, int end, int crossZoneCost) {
+    int *dist = new int[locations];
+    int *visited = new int[locations];
+
+    for (int i = 0; i < locations; i++) {
+        dist[i] = 1e9;
+        visited[i] = 0;
+    }
+
+    dist[start] = 0;
+
+    for (int k = 0; k < locations - 1; k++) {
+        int u = -1;
+        int minDist = 1e9;
+
+        for (int i = 0; i < locations; i++) {
+            if (!visited[i] && dist[i] < minDist) {
+                minDist = dist[i];
+                u = i;
+            }
+        }
+
+        if (u == -1) break;
+        visited[u] = 1;
+
+        for (int v = 0; v < locations; v++) {
+            if (roads[u][v] != 0 && !visited[v]) {
+
+                int extra = 0;
+                if (zoneOfLocation[u] != zoneOfLocation[v]) {
+                    extra = crossZoneCost;
+                }
+
+                int newDist = dist[u] + roads[u][v] + extra;
+                if (newDist < dist[v]) {
+                    dist[v] = newDist;
+                }
+            }
+        }
+    }
+
+    int ans = dist[end];
+    delete[] dist;
+    delete[] visited;
+    return ans;
+}
+
 City::~City() {
     for (int i = 0; i < locations; i++) {
         delete[] roads[i];
     }
     delete[] roads;
-}
 
+    delete[] zoneOfLocation;
+}
